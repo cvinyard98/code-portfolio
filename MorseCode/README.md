7b10af668e13b21f83cd9f138f7ce7614d6a3f8e
cvinyard98 <91570431+cvinyard98@users.noreply.github.com>
2022-03-05 21:37:17 -0700

    added morse code in rush

---
+++ MorseCode/README.md
@@ -0,0 +1,21 @@
+# Overview
+
+This program converts between letters and morse code. It can translate in either direction, from English into Morse code and from Morse code into English. 
+
+[Software Demo Video](https://youtu.be/y8sQuyygmDw)
+
+# Development Environment
+
+This program was developed in Rust using the tutorialspoint online compiler.
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [TutorialsPoint](https://www.tutorialspoint.com/rust/rust_input_output.htm)
+* [RustLang](https://users.rust-lang.org/)
+*[GeeksForGeeks](https://www.geeksforgeeks.org/standard-i-o-in-rust/)
+
+# Future Work
+
+* Better error handling to prevent crashes if they have bad input
+* Better use of Crates
---
+++ MorseCode/morse_code.rs
@@ -0,0 +1,97 @@
+use std::collections::HashMap;
+use std::io;
+
+
+fn setup_morse(morse:&mut HashMap<String, String>){
+    morse.insert(String::from(".-"), String::from("a"));
+    morse.insert(String::from("-..."), String::from("b"));
+    morse.insert(String::from("-.-."), String::from("c"));
+    morse.insert(String::from("-.."), String::from("d"));
+    morse.insert(String::from("."), String::from("e"));
+    morse.insert(String::from("..-."), String::from("f"));
+    morse.insert(String::from("--."), String::from("g"));
+    morse.insert(String::from("...."), String::from("h"));
+    morse.insert(String::from(".."), String::from("i"));
+    morse.insert(String::from(".---"), String::from("j"));
+    morse.insert(String::from("-.-"), String::from("k"));
+    morse.insert(String::from(".-.."), String::from("l"));
+    morse.insert(String::from("--"), String::from("m"));
+    morse.insert(String::from("-."), String::from("n"));
+    morse.insert(String::from("---"), String::from("o"));
+    morse.insert(String::from(".--."), String::from("p"));
+    morse.insert(String::from("--.-"), String::from("q"));
+    morse.insert(String::from(".-."), String::from("r"));
+    morse.insert(String::from("..."), String::from("s"));
+    morse.insert(String::from("-"), String::from("t"));
+    morse.insert(String::from("..-"), String::from("u"));
+    morse.insert(String::from("...-"), String::from("v"));
+    morse.insert(String::from(".--"), String::from("w"));
+    morse.insert(String::from("-..-"), String::from("x"));
+    morse.insert(String::from("-.--"), String::from("y"));
+    morse.insert(String::from("--.."), String::from("z"));
+    morse.insert(String::from(".-.-.-"), String::from("."));
+    morse.insert(String::from("..--.."), String::from("?"));
+    morse.insert(String::from("..-.."), String::from(","));
+}
+
+fn eng_to_morse(line:String, morse:HashMap<String, String>){
+    let mut result = String::new();
+    
+    for i in line.chars(){
+        for (key, val) in morse.iter(){
+            if i == val.chars().next().unwrap(){
+                for j in key.chars(){
+                    result.push(j);
+                }
+                result.push(' ');
+            }
+        }
+    }
+    println!("{}", result);
+}
+
+fn morse_to_eng(line:String, morse:HashMap<String, String>){
+    let mut result = String::new();
+    let mut temp = String::new();
+    for i in line.chars(){
+        if i != ' '{
+            temp.push(i);
+        }
+        else{
+            
+            result.push(morse[&temp].chars().next().unwrap());
+            temp = String::new();
+            
+        }
+    }
+    println!("{}", result);
+}
+
+
+
+fn main() {
+    let mut morse = HashMap::new();
+    setup_morse(&mut morse);
+
+    let mut eng_morse = String::new();
+    println!("Are you translating from english to morse code (1) or morse code to english(2)?");
+    io::stdin().read_line(&mut eng_morse).expect("Error reading input");
+    
+    
+    let mut line = String::new();
+    println!("please enter what you want translated: ");
+    io::stdin().read_line(&mut line).expect("Error reading input");
+    
+    if eng_morse.trim() == "1"{
+        eng_to_morse(line, morse);
+    }
+    else if eng_morse.trim() == "2"{
+        morse_to_eng(line, morse);        
+    }
+    else{
+        println!("Bad Input!");
+    }
+
+}
+
+
